9ff6ab7c758221f34275c20d9db8ca6f8fc0d1aa
nnnnn81 <60097331+nnnnn81@users.noreply.github.com>
2023-06-18 08:17:12 +0900

    Update login-main.tsx

--- src/app/components/login-main.tsx
+++ src/app/components/login-main.tsx
@@ -52,7 +52,6 @@ export default function Main() {
             sign up
           </button>
           <button
-            onClick={LogIn}
             className="mt-3 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
           >
             log in
